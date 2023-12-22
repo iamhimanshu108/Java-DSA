ea5f9fdc0927a4cef5a4397d27b3d66e34d374fb
Himanshu Kumar (Yadav) <iamhimanshu108@gmail.com>
2023-12-22 13:38:44 +0530

    Variables

---
+++ Basics/updatingVariables.java
@@ -0,0 +1,8 @@
+package Basics;
+
+public class updatingVariables {
+    public static void main(String[] args) {
+        int x;
+        x = 8;
+    }
+}
---
+++ Basics/variables.java
@@ -0,0 +1,13 @@
+package Basics;
+
+public class variables {
+    public static void main(String[] args) {
+        int x;  // declaration
+        x = 6; // initialization
+        int y  = 10; // declaration & initialization
+        
+        System.out.println(x);
+        System.out.println(x+y);
+
+    }
+}
