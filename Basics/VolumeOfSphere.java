bf9b114847fcd2c9f3c338c829e7c1389096041f
Himanshu Kumar (Yadav) <iamhimanshu108@gmail.com>
2023-12-23 23:18:38 +0530

    Volume of Sphare

---
+++ Basics/VolumeOfSphere.java
@@ -0,0 +1,9 @@
+package Basics;
+
+public class VolumeOfSphere {
+    public static void main(String[] args) {
+        double r = 3.8;
+        double v = 3.14 *r*r*r * 4/3;
+        System.out.println(v);
+    }
+}
