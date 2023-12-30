4411b8d87cbc6c62ffa4d6d2be9f8ca32c1de4d8
Himanshu Kumar (Yadav) <iamhimanshu108@gmail.com>
2023-12-30 19:39:18 +0530

    Equality

---
+++ Conditionals/Even_Odd/equalityOperator.java
@@ -0,0 +1,14 @@
+package Conditionals.Even_Odd;
+
+public class equalityOperator {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
+        // System.out.println(a==b);
+        // System.out.println(a>b);
+        // System.out.println(a<b);
+           System.out.println(a>=b);
+            System.out.println(a<=b);
+     }
+    
+}
