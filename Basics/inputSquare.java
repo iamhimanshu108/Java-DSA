fdc60ec2eee6155bf71d96004876707ab20d1cb7
Himanshu Kumar (Yadav) <iamhimanshu108@gmail.com>
2023-12-30 15:29:52 +0530

    Area of Sqaure

---
+++ Basics/inputSquare.java
@@ -0,0 +1,10 @@
+package Basics;
+import java.util.Scanner;
+public class inputSquare {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a Number : ");
+        int n = sc.nextInt();
+        System.out.println(n*n);
+    }
+}
---
+++ Basics/variableNamingRules.java
@@ -0,0 +1,9 @@
+package Basics;
+
+public class variableNamingRules {
+    public static void main(String[] args) {
+        int x = 6;
+        System.out.println(x);
+
+    }
+}
