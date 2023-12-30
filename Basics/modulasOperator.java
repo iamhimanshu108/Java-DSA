9d55081ab7175b6c2d8db1e200cabe84a086a483
Himanshu Kumar (Yadav) <iamhimanshu108@gmail.com>
2023-12-30 15:29:18 +0530

    Modulas

---
+++ Basics/modulasOperator.java
@@ -0,0 +1,10 @@
+package Basics;
+
+public class modulasOperator {
+    public static void main(String[] args) {
+        int a =8;
+        int b =2;
+        System.out.println(a%b);
+    }
+}
+    
