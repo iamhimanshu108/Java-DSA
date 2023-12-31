a9c8ffd8138eb9196222fd615ba30d5ffc2b5ac5
Himanshu Kumar (Yadav) <iamhimanshu108@gmail.com>
2023-12-31 19:08:33 +0530

    Volume

--- Basics/VolumeOfSphere.java
+++ Basics/VolumeOfSphere.java
@@ -2,7 +2,7 @@ package Basics;
 
 public class VolumeOfSphere {
     public static void main(String[] args) {
-        double r = 3.8;
+        double r = 10;
         double v = 3.14 *r*r*r * 4/3;
         System.out.println(v);
     }
