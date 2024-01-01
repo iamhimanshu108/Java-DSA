7076112afc31b3fd99a3b95157b2000a5bee2415
Himanshu Kumar (Yadav) <iamhimanshu108@gmail.com>
2024-01-01 22:34:44 +0530

    Area of circle

--- Basics/AreaofCircle.java
+++ Basics/AreaofCircle.java
@@ -2,7 +2,7 @@ package Basics;
 
 public class AreaofCircle {
     public static void main(String[] args) {
-        double r = 8;
+        double r = 7.8;
         double a = 3.14 * r *r ;
         System.out.println("Area of Circle is : " +a);
         System.out.println("Area of Circle with radius "+r+" is : " +a);
