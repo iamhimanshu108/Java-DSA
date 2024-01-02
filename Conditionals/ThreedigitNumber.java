83a569edb0bbb5f9b1e2acd6bf781c35b5e6ade7
Himanshu Kumar (Yadav) <iamhimanshu108@gmail.com>
2024-01-02 18:45:30 +0530

    Three digit Number

--- Conditionals/ThreedigitNumber.java
+++ Conditionals/ThreedigitNumber.java
@@ -1,5 +1,16 @@
+// Q:- Take positive integer input and tell if it ia a three digit nmber or not.
+
 package Conditionals;
 
+import java.util.Scanner;
+
 public class ThreedigitNumber {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Cost Price : ");
+        int n = sc.nextInt();
+
+        if(99<n && n<1000)System.out.println("3 Digit Number");
+        else System.out.println("Not a 3 digit Number");
+    }
 }
