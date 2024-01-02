5deeacf0362a3d072ba234dbb01ece98e7f146b8
Himanshu Kumar (Yadav) <iamhimanshu108@gmail.com>
2024-01-02 16:35:35 +0530

    Profit Loss

---
+++ Conditionals/Even_Odd/profitLoss.java
@@ -0,0 +1,20 @@
+package Conditionals.Even_Odd;
+
+import java.util.Scanner;
+
+public class profitLoss {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Cost Price : ");
+        int cp = sc.nextInt();
+        System.out.print("Enter sell Price : ");
+        int sp = sc.nextInt();
+
+        if(sp > cp){
+            System.out.print("Profit is ");
+            System.out.println(sp-cp);
+        } 
+        if(cp > sp) System.out.println("Loss is " +(cp-sp));
+        if(cp == sp) System.out.println("Kam Chal Gya");
+    }
+}
