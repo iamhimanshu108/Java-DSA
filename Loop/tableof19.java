6a38780714b2d96b758aa30f811cd94c417b5131
Himanshu Kumar (Yadav) <iamhimanshu108@gmail.com>
2024-01-08 19:16:13 +0530

    tableof19

---
+++ Loop/tableof19.java
@@ -0,0 +1,9 @@
+package Loop;
+
+public class tableof19 {
+    public static void main(String[] args) {
+        for(int i=19; i<=190; i+=19){
+            System.out.println(i);
+        }
+    }
+}
