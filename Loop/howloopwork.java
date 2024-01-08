0bb9185b110956bd0555cafd5f3dec58f4fb5050
Himanshu Kumar (Yadav) <iamhimanshu108@gmail.com>
2024-01-08 13:30:32 +0530

    workingofLoop

---
+++ Loop/howloopwork.java
@@ -0,0 +1,10 @@
+package Loop;
+
+public class howloopwork {
+    public static void main(String[] args) {
+        for(int i=10; i<=14; i++){
+            System.out.println("Hello");
+        }
+        
+    }
+}
