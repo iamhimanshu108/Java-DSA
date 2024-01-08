1355969b0a450c28a9e58c94a1d1d935ae76f379
Himanshu Kumar (Yadav) <iamhimanshu108@gmail.com>
2024-01-08 19:00:57 +0530

    EvenOdd

--- Conditionals/Even_Odd/EvenOdd.java
+++ Conditionals/Even_Odd/EvenOdd.java
@@ -4,13 +4,14 @@ import java.util.Scanner;
 
 public class EvenOdd {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter The Number : ");
-        int n = sc.nextInt();
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter The Number : ");
+            int n = sc.nextInt();
 
-        if(n%2 == 0)System.out.println("Even");
-        if(n%2 == 1)System.out.println("Odd");
-        // if(n%2 != 0)System.out.println("Odd");
+            if(n%2 == 0)System.out.println("Even");
+            if(n%2 == 1)System.out.println("Odd");
+            // if(n%2 != 0)System.out.println("Odd");
+        }
     }
     
 }
