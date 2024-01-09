720eca871875f7c27830cc86efe0c9f434805518
Himanshu Kumar (Yadav) <iamhimanshu108@gmail.com>
2024-01-09 17:57:55 +0530

    WhileLOOP

---
+++ Loop/whileLoop.java
@@ -0,0 +1,16 @@
+package Loop;
+
+public class whileLoop {
+    public static void main(String[] args) {
+        // for (int i = 1; i <=10; i++) {
+        //     System.out.println(i);
+        // }
+
+        int i=1;
+        while (i<=10) {
+            System.out.println(i);
+            i++;
+            
+        }
+    }
+}
