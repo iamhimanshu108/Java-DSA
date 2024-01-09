633a9f824ddc499fbf54ece44253e3bdb7ce0a4f
Himanshu Kumar (Yadav) <iamhimanshu108@gmail.com>
2024-01-09 17:44:16 +0530

    OddNumber

---
+++ Loop/oddNumberPrint.java
@@ -0,0 +1,10 @@
+package Loop;
+
+public class oddNumberPrint {
+    public static void main(String[] args) {
+        for (int i = 1; i <=100; i++) {
+            if(i%2!=0) System.out.println(i+"");
+            
+        }
+    }
+}
