c06985f647f0a4a21fa2831fc5f672ee350ed8bf
Himanshu Kumar (Yadav) <iamhimanshu108@gmail.com>
2024-01-10 12:58:19 +0530

    Dowhile LOoP

---
+++ Loop/DowhileLoop.java
@@ -0,0 +1,19 @@
+package Loop;
+
+public class DowhileLoop {
+    public static void main(String[] args) {
+        // int i=1;
+        // while (i<=10) {
+        //     System.out.println(i);
+        //     i++;
+            
+        // }
+        
+        int i =1;
+        do{
+            System.out.println(i);
+            i++;
+
+        }while(i<=10);
+    }
+}
