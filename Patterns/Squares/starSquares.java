0a195f9b6c03e04e5ed52fde8376a1741f296e08
Himanshu Kumar (Yadav) <iamhimanshu108@gmail.com>
2024-01-10 17:10:11 +0530

    StarSqaure

---
+++ Patterns/Squares/starSquares.java
@@ -0,0 +1,18 @@
+package Patterns.Squares;
+
+import java.util.Scanner;
+
+public class starSquares {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter rows & Cols : ");
+        int m = sc.nextInt();
+        // int n = sc.nextInt();
+        for (int i = 1; i <=m; i++) { //rows
+            for(int j=1; j<=m;j++){ //cols
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
