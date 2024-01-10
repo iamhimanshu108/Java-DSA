97ea66849eacaa3fa493b35e0df648bd00c49f5e
Himanshu Kumar (Yadav) <iamhimanshu108@gmail.com>
2024-01-10 19:09:04 +0530

    starTraingle

---
+++ Patterns/triangles/starTriangle.java
@@ -0,0 +1,17 @@
+package Patterns.triangles;
+
+import java.util.Scanner;
+
+public class starTriangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your n: ");
+        int n = sc.nextInt();
+        for (int i = 1; i <=n; i++) { //rows
+            for(int j=1; j<=i;j++){ //cols
+                System.out.print("*" + " ");
+            }
+            System.out.println();
+        }
+    }
+}
