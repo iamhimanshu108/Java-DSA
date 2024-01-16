e9667538bae5e31e0e9300f968f7e31f9b8e9ac1
Himanshu Kumar (Yadav) <iamhimanshu108@gmail.com>
2024-01-16 16:13:14 +0530

    Arrays

---
+++ Arrays/basicSyntax.java
@@ -0,0 +1,20 @@
+package Arrays;
+
+public class basicSyntax {
+    public static void main(String[] args) {
+        
+        int[] arr = new int[5];
+        // initialising individuals elements
+        arr[0] = 100;
+        arr[1] = 20;
+        arr[2] = 30;
+        arr[3] = 40;
+        arr[4] = 50;
+        // output of array elements
+        System.out.println(arr[0] + " ");
+        // updates
+        arr[0] = 99; 
+        arr[0] += 10; 
+        System.out.println(arr[0]);
+    }
+}
---
+++ Arrays/initialiseArray.java
@@ -0,0 +1,12 @@
+package Arrays;
+
+public class initialiseArray {
+    public static void main(String[] args) {
+        int[] arr = {10,20,30,40,50,60,-5,54,-78,96,25,78,36,63,};
+        int n = arr.length;
+        System.out.println(n);
+        for (int i=0; i<n; i++){
+            System.out.println(arr[i]+ " ");
+        }
+    }
+}
