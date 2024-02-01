a7a6d28afc568020bcaeb8048913a47f0864bf18
Himanshu Kumar (Yadav) <iamhimanshu108@gmail.com>
2024-02-01 20:25:23 +0530

    interning

---
+++ strings/interning.java
@@ -0,0 +1,11 @@
+package strings;
+
+public class interning {
+    public static void main(String[] args) {
+        String s = "Himanshu";
+        String q  = "Hiamnshu";
+        // Himanshu ->Timanshu
+        // s = "Timanshu";
+        System.out.println(s);
+    }
+}
