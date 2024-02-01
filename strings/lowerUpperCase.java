4e7a6a7ff88986affc4dbf4609af026822a8d583
Himanshu Kumar (Yadav) <iamhimanshu108@gmail.com>
2024-02-01 19:31:25 +0530

    lower,UpperCase

---
+++ strings/lowerUpperCase.java
@@ -0,0 +1,18 @@
+package strings;
+
+public class lowerUpperCase {
+    public static void main(String[] args) {
+        String s  = "Himanshu Yadav is 21 Years Old";
+        // System.out.println(s.toLowerCase());
+        // s.toLowerCase();  -> Nothing will happen
+        s = s.toLowerCase();
+        System.out.println(s);
+        System.out.println(s.toUpperCase());
+
+        String a  = "abc";
+        String b ="xyz";
+        // a.concat(b); Do Nothing
+        // System.out.println(a.concat(b));
+        
+    }
+}
