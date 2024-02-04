0cf871efdb6899691ad15511e1b5c77166d98bfc
Himanshu Kumar (Yadav) <iamhimanshu108@gmail.com>
2024-02-04 12:22:07 +0530

    sorting

---
+++ strings/sorting.java
@@ -0,0 +1,17 @@
+package strings;
+
+import java.util.Arrays;
+
+public class sorting {
+    public static void main(String[] args) {
+        String s = "himanshu";
+        char[] ch  = s.toCharArray();
+        for(char ele : ch){
+            System.out.println(ele);
+        }
+        System.out.println();
+        
+        StringBuilder sb  = new StringBuilder("garg");
+        char[] arr = sb.toString().toCharArray();
+    }
+}
